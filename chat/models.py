de23ef182c72fa996c059d572e12cc5c8dd9a262
pedromellodev <pedromello19122006@gmail.com>
2024-04-22 11:59:51 -0300

    Realizando migrações

--- chat/models.py
+++ chat/models.py
@@ -9,7 +9,14 @@ class Room(models.Model):
         on_delete = models.CASCADE,
     )
 
-    title = models.Charfield(max_length=200)
+    title = models.CharField(max_length=200)
     messages = models.ManyToManyField('message')
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
